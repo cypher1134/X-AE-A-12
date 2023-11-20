0c2ff8181737f8d24376ed6a606d89b5fbd168bb
alexandre.baux@student-cs.fr <alexandre.baux@student-cs.fr>
2023-11-20 11:17:02 +0100

    Creation of the dashboard

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-from src import data
+from src import data, dashboard
 import pandas as pd
 import sqlite3
-
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+import plotly.express as px
+import time
 
 if __name__ == "__main__":
     db_conn = sqlite3.connect("data/scrap.db")
     db_cursor = db_conn.cursor()
     raw_data = data.db_to_dataframe(db_cursor)
     raw_data['date'] = raw_data['date'].apply(data.twi_time_to_unix)
-    print(raw_data)
+
+    load_figure_template("darkly")
+    dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+    app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY, dbc_css])
+
+    app.layout = html.Div([
+        dbc.Row([
+            dbc.Progress(value=50, id="progressbar_init"),
+            dcc.Interval(id='clock', interval=1000, n_intervals=0, max_intervals=-1),
+            dbc.InputGroup([
+                dbc.InputGroupText("Search"), 
+                dbc.Input(id="search", type="text", placeholder="Type something...", debounce=True)
+            ], className="mb-3"),
+        ]),
+        dbc.Row([
+            dbc.Col([dash_table.DataTable(data=raw_data.to_dict('records'), page_size=10, id='table', virtualization=True)], className="dbc", width=6),
+            dbc.Col([
+                dcc.Graph(figure={}, id='tweet_count'),
+                dcc.Graph(figure={}, id='polarity'),
+                dbc.Card("This is also within a body", body=True),
+            ], width=5)
+        ]),
+    ])
+    app.run(debug = True)
+
+    @app.callback(
+    Input("clock", "n_intervals"),
+    Output("progressbar_init", "value"),
+    )
+    def progress_bar_update(n):
+        return(data.init_percent * 100,)
--- src/data.py
+++ src/data.py
@@ -4,14 +4,20 @@ from tqdm import tqdm
 import time
 from datetime import datetime
 
+init_percent = 0
 
 def db_to_dataframe(cursor):
     data=pd.DataFrame(columns=['id','user','text','view','like','retweet','date'])
     aide=None
     cursor.execute("SELECT Count() FROM tweets")
     n=cursor.fetchone()[0]
+    ###################################
+    n=1000
+    ###################################
     cursor.execute('SELECT * FROM tweets')
+    global init_percent
     for i in tqdm(range(n)):
+        init_percent = round(i/n-1, 2)
         aide = cursor.fetchone()
         aide = [0 if v is None else v for v in aide]
         data.loc[i] = aide
