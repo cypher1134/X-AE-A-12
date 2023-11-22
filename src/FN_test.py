fa9c19bf7ba85fcc3dde364c1e308c654e4f2bab
Philemon Wehbe <philemon@Philemons-MacBook-Pro.local>
2023-11-22 21:05:14 +0100

    corrected ML tests

--- src/FN_test.py
+++ src/FN_test.py
@@ -1,29 +1,27 @@
-import FN
-import data
+import numpy as np
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Test on the same database of training should get a 100% accuracy
-predicted_full_train_df = FN.predict_on_database("../data/train.csv", "../data/train.csv")
+import FN  # Assuming this is a module you've defined
 
-# Calculate accuracy on full training data
-accuracy_full_train = accuracy_score(predicted_full_train_df["fake_value"], pd.read_csv("../data/train.csv",nrows=data.n)["fake_value"])
-print(f"Accuracy on full training data: {accuracy_full_train * 100:.2f}%")
 
 # Load the full training dataset
-full_train_df = pd.read_csv("../data/train.csv",nrows=data.n)
+df_Full_test = pd.read_csv("../data/train.csv")
 
 # Split the training dataset into train and test sets
-partial_train_df, partial_test_df = train_test_split(full_train_df, test_size=0.4, random_state=42)
+partial_train_df, partial_test_df = train_test_split(df_Full_test, test_size=0.8 )
+partial_test_df_cmp=partial_test_df.copy()
 
 # Save the smaller training and test datasets to new files
 partial_train_df.to_csv("../data/partial_train.csv", index=False)
 partial_test_df.to_csv("../data/partial_test.csv", index=False)
 
 # Test on the partial test dataset
-predicted_partial_test_df = FN.predict_on_database("../data/partial_test.csv", "../data/partial_train.csv")
+predicted_partial_test_df = FN.predict_on_database(partial_test_df, "../data/partial_train.csv")
 
 # Calculate accuracy on partial test data
-accuracy_partial_test = accuracy_score(predicted_partial_test_df["fake_value"], pd.read_csv("../data/partial_test.csv")["fake_value"])
+accuracy_partial_test = accuracy_score(predicted_partial_test_df["fake_value"], partial_test_df_cmp["fake_value"])
 print(f"Accuracy on partial test data: {accuracy_partial_test * 100:.2f}%")
