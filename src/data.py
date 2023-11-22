cde2138e2e1c302317cc3c23dcd8513f2a54993c
alexandre.baux@student-cs.fr <alexandre.baux@student-cs.fr>
2023-11-22 16:14:04 +0100

    minor fixes

--- src/data.py
+++ src/data.py
@@ -2,18 +2,18 @@ import pandas as pd
 import sqlite3
 from tqdm import tqdm
 import time
-n=0
 
+
+n=0
 init_percent = 0
 raw_data = None
 
-def db_thread(path, savepath="../data/raw_data.json", force_writing=False):
+def db_thread(path, savepath="./data/raw_data.json", force_writing=False):
     global raw_data
     global init_percent
     if not force_writing:
         try :
-            raw_data=pd.read_json(savepath)
-            print(raw_data)
+            raw_data=pd.read_json(savepath, convert_dates=False)
             print('-----Finished to load raw_data-----')
         except Exception as e:
             print(e)
@@ -23,8 +23,8 @@ def db_thread(path, savepath="../data/raw_data.json", force_writing=False):
         db_cursor = db_conn.cursor()
         raw_data = db_to_dataframe(db_cursor)
         raw_data['date'] = raw_data['date'].apply(twi_time_to_unix)
-        raw_data.to_json(savepath)
         raw_data['fake_value'] = update_fake_value(raw_data)  
+        raw_data.to_json(savepath)
         print('-----Raw_data registered-----')
     init_percent = 100
 
@@ -48,7 +48,4 @@ def twi_time_to_unix(time_str):
     return time.mktime(time.strptime(time_str, "%Y-%m-%d %H:%M:%S+00:00"))
 
 def update_fake_value(data):
-# Add your logic here to update the 'fake_value' column based on some criteria
-# For now, setting it to 0 for all rows as an example
     return ['FAKE'] * len(data)
-    
