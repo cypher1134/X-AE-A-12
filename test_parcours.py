9164f532aeefccf32b3187dce82e7f868a067c7a
Dinnoo Sohel <sohel.dinnoo@student-cs.fr>
2023-11-23 11:09:08 +0100

    test parcours

---
+++ test_parcours.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from parcours import parcours
+import pytest
+
+
+def test_parcours():
+    # Test 1
+    graph1 = {'A': (5,[('B', 1), ('C', 1)],0.7), 'B': (1,[('A', 1)],0.7), 'C': (0.2,[('A', 1)],1)}
+    res1 = parcours(graph1, 'A')
+    expected_res1 = {'C': (0.2, [('A', 1)], 1),'A': (5, [('B', 1), ('C', 1)], 0.7),'B': (1, [('A', 1)], 0.7)}
+    assert res1 == expected_res1
+
+    # Test 2
+    graph2 = {'X': (1,[('Y', 1)],0.6), 'Y': (2,[('X', 1)],0.5), 'Z': (0,[],0)}
+    res2 = parcours(graph2, 'X')
+    expected_res2 = {'Y': (2, [('X', 1)], 0.5), 'X': (1, [('Y', 1)], 0.6)}
+    assert res2 == expected_res2
+
+    #Test 3
+    graph3 = {'X': (1,[('Y', 1)],0.6), 'Y': (2,[('X', 1)],0.5), 'Z': (0,[],0)}
+    with pytest.raises(KeyError):
+        parcours(graph3, 'A')
